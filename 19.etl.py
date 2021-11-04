d2f71e5438b430f116b8ea6e3c7f79d70a483e24
GuillaumeHerssens <89980132+GuillaumeHerssens@users.noreply.github.com>
2021-11-04 17:48:36 +0100

    Update 19.etl.py

--- 19.etl.py
+++ 19.etl.py
@@ -5,12 +5,11 @@ def transform(one,two,three,four,five,eight,ten):
     scrabble = {}
     old_scrabble = [one,two,three,four,five,eight,ten]
     for i in az:
-        #scrabble.append(i)
         for j in old_scrabble:
             if i.upper() in j:
                 score = old_scrabble.index(j) + 1
                 if score == 6:
-                    score = 7
+                    score = 8
                 elif score == 7:
                     score = 10
         scrabble[i] = score
