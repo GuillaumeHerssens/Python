46a029f94dc983a85afef5c97e0122fe47ae7506
GuillaumeHerssens <Guillaume.Herssens@gmail.com>
2021-11-04 10:19:44 +0100

    14 & 15

---
+++ 15.garden.py
@@ -0,0 +1,12 @@
+def garden(children, rows, seeds):
+    child = input("Name of child :")
+    pos = (children.index(child.lower()) * 2)
+    for i in rows:
+        cups = i[pos:pos+2]
+        for o, r in seeds.items():
+            cups = cups.replace(o, r)
+        print ("%s has cups containing %s on the %s row." % (child, cups.strip().replace(" ", " and "),str(rows.index(i)).replace("0", "first").replace("1", "second")))
+children = ["alice", "bob", "charlie", "david", "eve", "fred", "ginny", "harriet", "ileana", "joseph", "kincaid", "larry"]
+rows = ["VRCGVVRVCGGCCGVRGCVCGCGV", "VRCCCGCRRGVCGCRVVCVGCGCV"]
+seeds = {"V" : "Violet ", "R" : "Radish ", "C" : "Clover ", "G" : "Grass "}
+garden(children, rows, seeds)
