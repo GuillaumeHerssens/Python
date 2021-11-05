e09f50d7c2b7e5cd6e3386a336969d89a5a946fa
GuillaumeHerssens <89980132+GuillaumeHerssens@users.noreply.github.com>
2021-11-05 13:46:26 +0100

    Update 20.pig.py
    forgot a break x)

--- 20.pig.py
+++ 20.pig.py
@@ -5,6 +5,7 @@ def latin(sentence):
         for j in i:
             if j in vowels:
                 piglatin += i + "ay "
+                break
             else:                
                 i = i[1:] + i[0]
     print (piglatin)
