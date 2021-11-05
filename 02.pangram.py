931609672d7e20358156407e5913135e1c701b34
GuillaumeHerssens <Guillaume.Herssens@gmail.com>
2021-11-05 16:33:14 +0100

    improvement exo 2 (pangram)

--- 02.pangram.py
+++ 02.pangram.py
@@ -1,20 +1,23 @@
 import string
 checklist = string.ascii_lowercase
+punctuation = string.punctuation + string.whitespace
 
 def pangram(sentence):
-    sentence2 = (sentence.replace(" ", "")).lower()
-    check = True
-    for i in range(len(checklist)):
-        if checklist[i] in sentence2:
+    
+    for x in punctuation:
+        sentence = (sentence.replace(x, "").lower())
+
+    for i in checklist:
+        if i in sentence:
+            if i == "z":
+                print ("The sentence is a pangram")
             continue
         else:
             print ("The sentence is not a pangram")
-            check = False
             break
 
-    if check == True:
-        print ("The sentence is a pangram")
+        
 
 
 
-pangram("The quick brown fox jumps over the lazy dog")
+pangram("The ,quick; brown,fox jumps ,over the %%%%lazy (dog).")
