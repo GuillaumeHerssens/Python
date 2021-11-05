7f64c95c5efa30c564aae5a7e05bb4f93e098718
GuillaumeHerssens <Guillaume.Herssens@gmail.com>
2021-11-05 20:45:11 +0100

    improvements exo 13

--- 13.summultiple.py
+++ 13.summultiple.py
@@ -1,24 +1,16 @@
 import math
-
-# sum of all the multiple until n
+# sum of all the multiple of any number until n (for fun)
 def multiplesum(n):
  for i in range(1,n):
     result = 0
-    x = 1
-    while (i * x) < n:
-        result += (i * x)
-        x += 1
-    if result == 0:
-        break
-    else:
-        print ("sum of all multiple of %d up to %d : %d" % (i, (n - 1), result))
-
-# sum of the multiple of given m until n
+    for j in range(math.ceil(n / i)):
+        result += (j * i)
+    print ("sum of all multiple of %d up to %d : %d" % (i, (n - 1), result))
+# sum of the all the multiple of given m until n
 def multiplesum_of_m(n,m):
     result = 0
-    for i in range (1,n):
-        if (i * m) < n:
-            result += (i * m)
+    for i in range(math.ceil(n / m)):
+        result += (i * m)
     print ("sum of all multiple of %d up to %d : %d" % (m, (n - 1), result))
 
 n = 10
